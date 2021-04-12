ce81789ae52341db4187573613e2f282b1976af8
andigandhi <andigrasser13@gmail.com>
2021-04-12 15:50:24 +0200

    Added Script and Pics

---
+++ script.js
@@ -0,0 +1,42 @@
+// Variablen
+var anzahlBilder = 5;
+var aktuellesBild = 0;
+
+// Zeige Bild auf den Koordinaten (w, h)
+function bildAnzeigen(w, h) {
+
+    // Das alte Bild löschen
+    if (document.getElementById("bild") != null) {
+        document.body.removeChild( document.getElementById("bild") );
+    }
+
+    // Ein neues Bild erstellen
+    var bild = document.createElement('img');
+
+    // Den korrekten Link zum Bild angeben
+    bild.src = "bilder/" + aktuellesBild.toString() + ".jpg";
+
+    // Ein "alt" Attribut angeben (wichtig für Screenreader für Sehgeschädigte)
+    bild.alt = "Ein sehr spannendes Bild";
+
+    // Dem Bild eine ID geben
+    bild.id = "bild";
+
+    // Das Bild positionieren
+    bild.style.marginLeft = w.toString() + "px";
+    bild.style.marginTop = h.toString() + "px";
+
+    // Das Bild dem HTML-Dokument hinzufügen
+    document.body.appendChild(bild);
+}
+
+
+// Warte auf Klick und zeige ein Bild an
+document.addEventListener("click", function(event){
+    // event.clientX und -Y geben die Koordinaten der Maus beim Klick an
+    bildAnzeigen(event.clientX, event.clientY);
+    
+    // Gleich auf das nächste Bild verweisen
+    // das % bedeutet "modulus"
+    aktuellesBild = (aktuellesBild + 1) % anzahlBilder;
+}); 
